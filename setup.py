767b9b80c67154114d95934dbcaa6c0ed65e7059
kumar <kumar.mcmillan@gmail.com>
2009-02-02 18:32:42 -0600

    beefed up setup file

--- setup.py
+++ setup.py
@@ -13,10 +13,23 @@ assert version
 setup(
     name='fudge',
     version=version,
-    description="",
-    long_description="",
+    description="Replace real objects with fakes (mocks, stubs, etc) while testing.",
+    long_description="""
+Complete documentation is available at http://farmdev.com/projects/fudge/
+
+Here is a quick preview of how you can test code that sends email without actually sending email::
+    
+    >>> import fudge
+    >>> SMTP = fudge.Fake('SMTP')
+    >>> SMTP = SMTP.expects('__init__')
+    >>> SMTP = SMTP.expects('connect')
+    >>> SMTP = SMTP.expects('sendmail').with_arg_count(3)
+    >>> SMTP = SMTP.expects('close')
+    
+""",
     author='Kumar McMillan',
     author_email='kumar.mcmillan@gmail.com',
+    license=open("./LICENSE.txt").read(),
     packages=find_packages(exclude=['ez_setup']),
     install_requires=[],
     tests_require=['nose', 'Sphinx'],
