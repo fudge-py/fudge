dd262a9a1d73a074ff046e00b394fb3eacdbb553
Kumar McMillan <kumar.mcmillan@gmail.com>
2009-03-18 10:45:06 -0500

    bumped fudge.js __version__ to 0.9.2 to keep up with the python lib (note that it does *not* implement the full 0.9.2 interface)

--- javascript/fudge/fudge.js
+++ javascript/fudge/fudge.js
@@ -444,7 +444,7 @@ fudge = function() {
     
     // fill fudge.* namespace :
     return {
-        '__version__': '0.9.1',
+        '__version__': '0.9.2',
         clear_expectations: function() { return registry.clear_expectations(); },
         ExpectedCall: ExpectedCall,
         Fake: Fake,
