827fd234b8acd9300457b2102e53c446f4ed9c5c
kumar <kumar.mcmillan@gmail.com>
2009-03-19 21:12:50 -0500

    better message for keywords not showing up

--- fudge/__init__.py
+++ fudge/__init__.py
@@ -265,8 +265,16 @@ class Call(object):
                         self, len(kwargs.keys()), self.expected_kwarg_count))
         
         return return_value
+        
+    def _args_are_equal(self, actual_args, expected_args):
+        pass
     
     def _keywords_are_equal(self, actual_kwargs, expected_kwargs):
+        """returns (True or False, reason)
+        
+        First value is True if keywords matched.  The second value is 
+        the reason.  It might be a blank string.
+        """
         expected_keys = set(expected_kwargs.keys())
         if (len(expected_keys)==1 and len(actual_kwargs.keys())==1):
             # no need for detailed messages
@@ -282,9 +290,10 @@ class Call(object):
                 return (False, "%s=%r != %s=%r" % (k, v, k, expected_kwargs[k]))
             expected_keys.remove(k)
         
-        if len(expected_keys):
-            return (False, "these keywords never showed up: %r" % tuple(expected_keys))
-            
+        exp_key_len = len(expected_keys)
+        if exp_key_len:
+            these = exp_key_len==1 and "this keyword" or "these keywords"
+            return (False, "%s never showed up: %r" % (these, tuple(expected_keys)))
         
         return (True, "")
 
--- fudge/tests/test_fudge.py
+++ fudge/tests/test_fudge.py
@@ -179,6 +179,13 @@ class TestKeywordArgEquality(unittest.TestCase):
         self.fake = Fake("foo")
         self.call = Call(self.fake)
     
+    def test_empty(self):
+        eq_(self.call._keywords_are_equal({}, {}), (True, ""))
+    
+    def test_one_empty(self):
+        eq_(self.call._keywords_are_equal({}, {'something':'here'}), 
+            (False, "this keyword never showed up: ('something',)"))
+    
     def test_complex_match_yields_no_reason(self):
         actual = {'num':1, 'two':2, 'three':3}
         expected = {'num':1, 'two':2, 'three':3}
@@ -210,7 +217,7 @@ class TestKeywordArgEquality(unittest.TestCase):
         actual = {'one':1}
         expected = {'one':1, 'two':2}
         eq_(self.call._keywords_are_equal(actual, expected), 
-            (False, "these keywords never showed up: 'two'"))
+            (False, "this keyword never showed up: ('two',)"))
     
     def test_expected_kw_value_inequal(self):
         actual = {'one':1, 'two':'not two'}
