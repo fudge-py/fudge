a93ca303de3659460ff54d7744d0ccf5ac6c5609
kumar <kumar.mcmillan@gmail.com>
2010-11-04 11:05:55 -0500

    Added test for string based patching of builtin modules

--- fudge/tests/test_patcher.py
+++ fudge/tests/test_patcher.py
@@ -14,7 +14,8 @@ def test_patch_obj():
     eq_(type(holder.exc), type(Freddie()))
     patched.restore()
     eq_(type(holder.exc), type(Exception()))
-    
+
+
 def test_patch_path():
     from os.path import join as orig_join
     patched = fudge.patch_object("os.path", "join", Freddie())
@@ -23,14 +24,31 @@ def test_patch_path():
     patched.restore()
     eq_(type(os.path.join), type(orig_join))
 
+
 def test_patch_builtin():
     import datetime
     orig_datetime = datetime.datetime
     now = datetime.datetime(2010, 11, 4, 8, 19, 11, 28778)
     fake = fudge.Fake('now', callable=True).returns(now)
     patched = fudge.patch_object(datetime.datetime, 'now', fake)
-    eq_(datetime.datetime.now(), now)
-    patched.restore()
+    try:
+        eq_(datetime.datetime.now(), now)
+    finally:
+        patched.restore()
+    eq_(datetime.datetime.now, orig_datetime.now)
+
+
+def test_patch_builtin_as_string():
+    import datetime
+    orig_datetime = datetime.datetime
+    now = datetime.datetime(2006, 11, 4, 8, 19, 11, 28778)
+    fake_dt = fudge.Fake('datetime').provides('now').returns(now)
+    patched = fudge.patch_object('datetime', 'datetime', fake_dt)
+    try:
+        # timetuple is a workaround for strange Jython behavior!
+        eq_(datetime.datetime.now().timetuple(), now.timetuple())
+    finally:
+        patched.restore()
     eq_(datetime.datetime.now, orig_datetime.now)
 
 
@@ -48,6 +66,7 @@ def test_decorator_on_def():
     some_test()
     eq_(holder.test_called, True)
     eq_(type(holder.exc), type(Exception()))
+
 
 def test_decorator_on_class():
     class holder:
@@ -67,6 +86,7 @@ def test_decorator_on_class():
     eq_(holder.test_called, True)
     eq_(type(holder.exc), type(Exception()))
 
+
 def test_patched_context():
     if not hasattr(fudge, "patched_context"):
         raise SkipTest("Cannot test with patched_context() because not in 2.5")
@@ -80,4 +100,3 @@ def test_patched_context():
     eq_(Boo.fargo, "is right here")
     ctx.__exit__(None, None, None)
     eq_(Boo.fargo, "is over there")
-    
